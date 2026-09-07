20d47c44ac467226a70df14ae0abc15a1eb82ae2
Rebeca Gomes <rebecadcgomes@gmail.com>
2026-09-07 17:47:08 -0300

    tentativa 1 - rodar analise de poder

--- calculo_amostralv1.R
+++ calculo_amostralv1.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load("dplyr")
+p_load("dplyr", "lme4", "simr")
 
 # matriz base (fase neutra pré e pós respiração)
 
@@ -21,7 +21,7 @@ dados_brutos <- expand.grid(
     Acerto = rbinom( # modela uma situação que só tem 2 saídas (acerto ou erro)
       n(), # quantos vezes precisará gerar um resultado (número de linhas esperado, 50)
       1, # número de tentativas por trial
-      prob = 0.85 # probabilidade de acerto; [ref]
+      prob = 0.95 # probabilidade de acerto; [ref]
       ),
     
     # simula 1 /\HRV fixo por fase e sessão , por participante
@@ -32,11 +32,30 @@ dados_brutos <- expand.grid(
       Fase == "Pos_Neutro" & Sessao == "Hiperventilacao" ~ rnorm(1, mean = -5, sd = 2) # [ref]
     ),
     
-    # simula o tempo de reação bruto
-    RT_Bruto = ifelse(
-      Tipo_Trial == "Repeticao", 
-      rnorm(n(), mean = 600, sd = 100), # [ref]
-      rnorm(n(), mean = 800, sd = 100)  # [ref]
+    # efeito da sessão no RT
+    Efeito_Sessao = case_when(
+      Fase == "Pre_Neutro" ~ 0,
+      Fase == "Pos_Neutro" & Sessao == "Box" ~ -30, # [ref]
+      Fase == "Pos_Neutro" & Sessao == "Prolongada" ~ -40, # [ref]
+      Fase == "Pos_Neutro" & Sessao == "Hiperventilacao" ~ +80 # [ref]
+    ),
+    
+    # efeito do hrv no RT
+    Efeito_HRV = case_when(
+      Fase == "Pre_Neutro" ~ 0,
+      Fase == "Pos_Neutro" ~ -2 * Delta_HRV # [ref]
+    ),
+    
+    Desvio_Individual = rnorm(1, mean = 0, sd = 30), # [ref]
+    
+    # simula o tempo de reação
+    RT = ifelse(
+      Tipo_Trial == "Repeticao",
+      600 + Desvio_Individual +
+        rlnorm(n(), meanlog = 3.5, sdlog = 0.5),
+      
+      800 + Desvio_Individual + Efeito_Sessao + Efeito_HRV +
+        rlnorm(n(), meanlog = 3.5, sdlog = 0.5)
     )
   ) %>%
   
@@ -44,12 +63,12 @@ dados_brutos <- expand.grid(
   mutate(
     Tipo_Anterior = lag(Tipo_Trial),
     Acerto_Anterior = lag(Acerto),
-    RT_Anterior = lag(RT_Bruto),
+    RT_Anterior = lag(RT),
     
     # calcula o custo de troca somente se...
     Switch_Cost = case_when(
       Tipo_Trial == "Troca" & Acerto == 1 & # o tipo atual de trial for um acerto de troca e...
-        Tipo_Anterior == "Repeticao" & Acerto_Anterior == 1 ~ RT_Bruto - RT_Anterior, # o anterior for um acerto de repetição
+        Tipo_Anterior == "Repeticao" & Acerto_Anterior == 1 ~ RT - RT_Anterior, # o anterior for um acerto de repetição
       TRUE ~ NA_real_ # O resto é descartado (NA)
     )
   ) %>%
@@ -58,3 +77,99 @@ dados_brutos <- expand.grid(
 
 head(dados_brutos)
 View(dados_brutos)
+
+# verificação
+
+dados_brutos %>%
+  group_by(Sessao) %>%
+  summarise(Media_Custo = mean(Switch_Cost, na.rm = TRUE))
+
+dados_brutos %>%
+  group_by(ID_Participante) %>%
+  summarise(
+    N_Switch = sum(!is.na(Switch_Cost))
+  )
+
+table(
+  dados_brutos$ID_Participante,
+  dados_brutos$Sessao,
+  dados_brutos$Fase
+)
+
+nrow(dados_brutos)
+
+n_distinct(dados_brutos$ID_Participante)
+
+sum(!is.na(dados_brutos$Switch_Cost))
+
+# modelo ===========
+
+# o custo de troca é explicado pelos efeitos fixos sessão e hrv, considerando o efeito aleatório do participante (intercepto)
+modelo_lmer <- lmer(
+  Switch_Cost ~ Sessao * Delta_HRV + (1 | ID_Participante),
+  data = dados_brutos
+)
+
+summary(modelo_lmer)
+VarCorr(modelo_lmer)
+
+
+# análise de poder simr ===========
+
+# parâmetros
+n_maximo <- 60
+efeito <- 0.3
+ICC <- 0.15
+
+# estende modelo pro n máximo
+modelo_estendido <- extend(
+  modelo_lmer,
+  along = "ID_Participante",
+  n = n_maximo)
+
+# ICC
+sigma_residual <- sigma(modelo_estendido)
+
+variancia_participante <- 
+  sigma_residual^2 * ICC / (1 - ICC)
+VarCorr(modelo_estendido)[["ID_Participante"]] <- variancia_participante
+
+
+# tamanho do efeito
+
+efeito_ms <- efeito * sigma_residual
+efeito_ms
+
+fixef(modelo_estendido)["SessaoProlongada:Delta_HRV"] <- -efeito_ms
+
+fixef(modelo_estendido)["SessaoHiperventilacao:Delta_HRV"] <- efeito_ms
+
+# verificar os parâmetros
+ICC_observado <- 
+  as.numeric(VarCorr(modelo_estendido)$ID_Participante) /
+  (
+    as.numeric(VarCorr(modelo_estendido)$ID_Participante) +
+      sigma(modelo_estendido)^2
+  )
+
+cat("Desvio-padrão residual:", sigma_residual, "\n")
+cat("Tamanho de efeito:", efeito, "\n")
+cat("Efeito em ms:", efeito_ms, "\n")
+cat("ICC:", ICC_observado, "\n")
+
+fixef(modelo_estendido)
+
+# análise de poder
+set.seed(45)
+
+curva_n_final <- powerCurve(
+  modelo_estendido,
+  test = fcompare(~ Sessao + Delta_HRV),
+  along = "ID_Participante",
+  nsim = 10
+)
+
+print(curva_n_final)
+plot(curva_n_final)
+
+head(curva_n_final$errors)
